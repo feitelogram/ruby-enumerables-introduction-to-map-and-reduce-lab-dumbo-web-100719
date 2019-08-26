3b1805918d4747a91a177ae13cbc62ca9e355dcf
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-26 14:51:34 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -6,11 +6,10 @@ array = [1,2,3,4]
 def average (array)
   total = 0 
   average = 0 
-  i= 0
-  array.each do
-    total += array[i]
+  array.each do |number|
+    total += number
+     binding.pry
   end
  average = total / array.length
  average
- binding.pry
 end
