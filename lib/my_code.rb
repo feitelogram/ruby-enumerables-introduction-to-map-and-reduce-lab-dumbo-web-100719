f4c4555ab015b1eee51897355ea10b5e3ed07d39
Nicholas Feitel <nicholas.feitel@gmail.com>
2019-08-26 14:49:53 +0000

    Automatically backed up by Learn

--- lib/my_code.rb
+++ lib/my_code.rb
@@ -1,1 +1,17 @@
 # My Code here....
+require "pry"
+
+array = [1,2,3,4]
+
+def average (array)
+  total = 0 
+  average = 0 
+  i= 0
+  array.length.times do
+    total += array[i]
+    i+=1
+  end
+ average = total / array.length
+ average
+ binding.pry
+end
